3619a02d03f21cef2cfb4437e6c8120bc537c4aa
jantopolovec <jantopolovec@github.com>
2020-01-07 12:22:58 +0100

    a
    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_120339) do
+ActiveRecord::Schema.define(version: 2020_01_07_105721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buildings", force: :cascade do |t|
+    t.string "ime"
+    t.integer "zvezdice"
+    t.text "opis"
+    t.string "naslov"
+    t.string "kraj"
+    t.string "tip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
